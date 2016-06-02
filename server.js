4728d523fccda89a780f34012194e957a63bcf65
JEZEQUEL <etienne.jezequel@capgemini.com>
2016-06-02 15:06:49 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+require( 'dotenv' ).load();
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const bodyParser = require('body-parser');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+
+//En fonction du mode (production ou développement), on intégre différentes fonctionnalités
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 8080 : process.env.PORT;
+
+//On appelle express permettant à Node JS de gérer l'application
+//comme par exemple l'écoute sur un port
+const app = express();
+
+if (isDeveloping) {
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', function response(req, res) {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.end();
+  });
+} else {
+  app.use(express.static(__dirname + '/dist'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+
+app.listen(port, function onStat(err){
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> Listening on port %s. Open up in your browser.',port);
+});
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'app/App.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'file?name=[path][name].[hash].[ext]'
+      }
+    ]
+  },
+}

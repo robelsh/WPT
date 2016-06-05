581a73ffb71904b70ae94b85cdf5400bb75f93e4
Etienne Jézéquel <EtienneJezequel@macbook-pro-de-etienne.home>
2016-06-05 12:19:24 +0200

    Ajout de material ui

--- app/containers/main/Main.js
+++ app/containers/main/Main.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import {Router,Link} from 'react-router';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 
 export default class Main extends Component {
   constructor(props){
     super(props);
-
   }
+  getChildContext() {
+    return {muiTheme: getMuiTheme(baseTheme)};
+  }
+
   render(){
     const dummySentences = ['Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'Donec hendrerit tempor tellus.', 'Donec pretium posuere tellus.', 'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Nulla posuere.', 'Donec vitae dolor.', 'Nullam tristique diam non turpis.', 'Cras placerat accumsan nulla.', 'Nullam rutrum.', 'Nam vestibulum accumsan nisl.'];
 
@@ -18,3 +24,7 @@ export default class Main extends Component {
     );
   }
 }
+
+Main.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
+};

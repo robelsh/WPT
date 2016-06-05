153cfbe8b434fadf9979873e28156dee4ceb180e
Etienne Jézéquel <EtienneJezequel@macbook-pro-de-etienne.home>
2016-06-05 12:52:07 +0200

    #1 Mise à jour, ajout de contrôle avant envoi vers firebase

--- app/components/Ask.js
+++ app/components/Ask.js
@@ -1,18 +1,31 @@
 import React, {Component} from "react";
-import {Button} from "react-bootstrap";
 import Paper from 'material-ui/Paper';
 import {List, ListItem} from 'material-ui/List';
 import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
+import Snackbar from 'material-ui/Snackbar';
+import Subheader from 'material-ui/Subheader';
 
 export class AnswersList extends Component {
   render() {
     return(
       <div>
-        {this.props.answers.map((item, index) =>
-          <List>
-            <ListItem>{item} <Button onTouchTap={this.props.delete} value={index}>X</Button></ListItem>
-          </List>
-        )}
+        <List>
+          <Subheader>Liste des réponses ({this.props.answers.length})</Subheader>
+          {this.props.answers.map((item, index) =>
+            <span key={index}>
+              <Divider/>
+              <ListItem
+                disabled={true}
+                insetChildren={true}
+                key={index}
+                primaryText={item}
+                rightIconButton={<FlatButton style={{margin:"5px"}} onTouchTap={(e)=>this.props.delete(index)} secondary={true}>Supprimer</FlatButton>}
+                />
+            </span>
+          )}
+        </List>
       </div>
     );
   }
@@ -24,11 +37,35 @@ export default class Ask extends Component{
     this.state = {
       answers:[],
       currentAnswer:"",
-      error:""
+      error:"",
+      question:"",
+      open:false,
+      message:""
     }
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeAnswer = this.handleChangeAnswer.bind(this);
+    this.handleChangeQuestion = this.handleChangeQuestion.bind(this);
     this.addAnswer = this.addAnswer.bind(this);
     this.deleteAnswer = this.deleteAnswer.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.addQuestion = this.addQuestion.bind(this);
+  }
+
+  addQuestion(){
+    console.log(this.state.answers.length);
+    if(this.state.question == ""){
+      this.setState({
+        open:true,
+        message:"Entrez une putain de question"
+      });
+    }
+    else if(this.state.answers.length < 2){
+      this.setState({
+        open:true,
+        message:"Entrez des putain de réponses"
+      });
+    }else{
+      console.log("ok");
+    }
   }
 
   addAnswer(e){
@@ -47,7 +84,7 @@ export default class Ask extends Component{
   }
 
   deleteAnswer(e){
-    var answerIndex = parseInt(e.target.value, 10);
+    var answerIndex = e;
     console.log(answerIndex);
     this.setState(state =>{
       state.answers.splice(answerIndex, 1);
@@ -55,22 +92,41 @@ export default class Ask extends Component{
     });
   }
 
-  handleChange(e){
+  handleChangeQuestion(e){
+    this.setState({
+      question:e.target.value,
+    });
+  }
+
+  handleChangeAnswer(e){
     this.setState({
       currentAnswer:e.target.value,
       error:""
     });
   }
 
+  handleRequestClose(){
+    this.setState({
+      open: false,
+    });
+  };
+
   render() {
     return(
       <Paper style={{padding:"34px"}} zDepth={1}>
+        <Snackbar
+          open={this.state.open}
+          message={this.state.message}
+          autoHideDuration={4000}
+          onRequestClose={this.handleRequestClose}
+          />
         <form>
-          <TextField type="text" hintText="Pose ta question" />
-          <TextField hintText="Réponse" type="text" value={this.state.currentAnswer} onChange={this.handleChange} errorText={this.state.error}
-/>
-          <Button onTouchTap={this.addAnswer}>Add</Button>
-          <AnswersList answers={this.state.answers.map((item)=>item)} delete={this.deleteAnswer}/>
+          <TextField type="text" value={this.state.question} onChange={this.handleChangeQuestion}  hintText="Pose ta question" /> <br/>
+          <TextField hintText="Réponse" type="text" value={this.state.currentAnswer} onChange={this.handleChangeAnswer} errorText={this.state.error}/><br/>
+          <FlatButton onTouchTap={this.addAnswer}>Add</FlatButton>
+          <AnswersList answers={this.state.answers.map((item)=>item)} delete={this.deleteAnswer}/><br/>
+          <Divider/>
+          <FlatButton onTouchTap={this.addQuestion}>Soumez la question</FlatButton>
         </form>
       </Paper>
     );
